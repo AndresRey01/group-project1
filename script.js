392be38133ca5aa94bdfc0a844ea82b2d1153d8c
Kevin Christensen <christensenkevin23@gmail.com>
2023-10-02 19:14:52 -0500

    added javascript

--- script.js
+++ script.js
@@ -1,4 +1,11 @@
 // Here is were our API's will go
-const apiUrl1 = ``
+const apiUrl1 = `1332bde37202c04762035c029a340211`
 const apiUrl2 = ``
 
+const getMarvelDetails = () => {
+    const marvelUrl = `https://gateway.marvel.com/v1/public/characters?apikey=${apiUrl1}`;
+}
+
+fetch(`https://gateway.marvel.com/v1/public/characters?apikey=${apiUrl1}`).then(res => res.json()).then(data =>{
+    console.log(data);
+})
